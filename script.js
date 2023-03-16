46015f8622e44f395c352a37a68944dcdca1eaba
dani morillo <danifromecuador@gmail.com>
2023-03-16 12:05:28 -0500

    Create first speaker dinamically

--- script.js
+++ script.js
@@ -15,6 +15,40 @@ mobileMenuClose.addEventListener('click', () => {
 });
 
 about.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hide');
-    navBar.classList.toggle('hide');
+  mobileMenu.classList.toggle('hide');
+  navBar.classList.toggle('hide');
+});
+
+const speakerData = [
+  {
+    name: 'John Smith',
+    position: 'university',
+    details: 'lorem',
+    photo: 'images/photos/photo1',
+  },
+  {
+    name: 'John Smith 2',
+    position: 'university 2',
+    details: 'lorem 2',
+    photo: 'images/photos/photo1 2',
+  },
+];
+
+const html = `
+  <div class="speaker">
+    <img src="" alt="" class="photo">
+    <div class="resume">
+      <div class="name">${speakerData[0].name}</div>
+      <div class="title">Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School</div>
+      <div class="horizontal-rule"></div>
+      <div class="description">Lorem ipsum dolor sit amet  asperiores eius! Iste fuga vel perspiciatis iusto?</div>
+    </div>
+  </div
+`;
+
+const newSpeaker = document.querySelector('.new-speaker');
+
+ham.addEventListener('click', () => {
+  newSpeaker.innerHTML = html;
+
 });
