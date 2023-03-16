c3dc7bb6af3ce417a3890ec117fc0ea98f658b12
dani morillo <danifromecuador@gmail.com>
2023-03-16 01:45:20 -0500

    Add anchor tag to about button in mobile menu

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const ham = document.querySelector('.ham');
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuClose = document.querySelector('.mobile-menu-close');
+const about = document.querySelector('#about-link');
 
 ham.addEventListener('click', () => {
   mobileMenu.classList.toggle('hide');
@@ -8,4 +9,9 @@ ham.addEventListener('click', () => {
 
 mobileMenuClose.addEventListener('click', () => {
   mobileMenu.classList.toggle('hide');
+});
+
+about.addEventListener('click', () => {
+    alert('About');
+    mobileMenu.classList.toggle('hide');
 });
