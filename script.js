274967fff1094704f52ee3bcd9c7bd7616e7e7b9
dani morillo <danifromecuador@gmail.com>
2023-03-16 11:27:10 -0500

    Add home and about functionallity

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const ham = document.querySelector('.ham');
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuClose = document.querySelector('.mobile-menu-close');
 const about = document.querySelector('#about-link');
+const navBar = document.querySelector('.nav-bar');
 
 ham.addEventListener('click', () => {
   mobileMenu.classList.toggle('hide');
+  navBar.classList.toggle('hide');
 });
 
 mobileMenuClose.addEventListener('click', () => {
   mobileMenu.classList.toggle('hide');
+  navBar.classList.toggle('hide');
 });
 
 about.addEventListener('click', () => {
     mobileMenu.classList.toggle('hide');
+    navBar.classList.toggle('hide');
 });
