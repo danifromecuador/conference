ac0f89902f663e41614c84507ef2a415ce49db9d
dani morillo <danifromecuador@gmail.com>
2023-03-17 12:09:26 -0500

    Avoid scrolling the content behind when the mobile menu is opened

--- script.js
+++ script.js
@@ -3,20 +3,24 @@ const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuClose = document.querySelector('.mobile-menu-close');
 const about = document.querySelector('#about-link');
 const navBar = document.querySelector('.nav-bar');
+const body = document.querySelector('.body');
 
 ham.addEventListener('click', () => { // open mobile menu and hide nav bar
   mobileMenu.classList.toggle('hide');
   navBar.classList.toggle('hide');
+  body.classList.toggle('mobile-menu-open');
 });
 
 mobileMenuClose.addEventListener('click', () => { // close mobile menu and show nav bar
   mobileMenu.classList.toggle('hide');
-  navBar.classList.toggle('hide');
+  navBar.classList.toggle('hide');  
+  body.classList.toggle('mobile-menu-open');
 });
 
 about.addEventListener('click', () => { // close mobile menu and show nav bar and navigate to about section
   mobileMenu.classList.toggle('hide');
-  navBar.classList.toggle('hide');
+  navBar.classList.toggle('hide');  
+  body.classList.toggle('mobile-menu-open');
 });
 
 const speakerData = [
